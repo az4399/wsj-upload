15fff754804512afca00a1fb0fdd2c472ff011b3
ayo <57715152+az4399@users.noreply.github.com>
2024-10-23 19:35:57 +0800

    Update download.py

--- download.py
+++ download.py
@@ -3,7 +3,22 @@ import requests
 def download_pdf():
     # 从api获取pdf文件的地址
     api_url = "https://customercenter.wsj.com/todaysPaper/"
-    api_response = requests.request("GET", api_url, allow_redirects=False).headers
+    headers1 = {
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'en-US,en;q=0.9',
+      'dnt': '1',
+      'priority': 'u=0, i',
+      'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+    }
+    api_response = requests.request("GET", api_url, allow_redirects=False, headers=headers1).headers
 
     print(api_response)
     # 从api的header返回的重定向地址
